bad683c8ae929c0ad4609e385cf94d7891be088b
Peter Trizuliak <peter.trizuliak@gmail.com>
2023-02-11 12:23:49 +0000

    restyle and add splitbee

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,19 @@
+export const AboutMe = () => (
+  <section id="about-me" className="px-16 mt-16 mb-16 md:mb-12">
+    <h1 className="py-3 text-5xl font-bold text-right align-middle">
+      Trizuliak
+    </h1>
+    <section className="text-lg text-right align-middle">
+      <p className="py-3">
+        Hello! 👋 I am Peter - software engineer, indie maker and dad - in
+        reverse order of importance.
+      </p>
+      {/* <p>
+        Internet is so vast and the chance of anyone reading this is so low,
+        that I can write here about anything, exactly the way I feel about it.
+        As if nobody is ever going to read it.
+      </p> */}
+      <p className="py-3">"Loving life." ? true : false;</p>
+    </section>
+  </section>
+);
---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+export const Nav = () => {
+  <nav>
+    <p>Home</p>
+    <p>Projects</p>
+    <p>Notes or Journal or Words or Thoughts or Blog</p>
+    <p>Quotes</p>
+    <p>Book Bits</p>
+  </nav>;
+};
